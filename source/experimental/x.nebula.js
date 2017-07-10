101c7ca8819f08583def25c96b108280c362c0d0
Delvar <Delvar@users.noreply.github.com>
2017-07-10 16:41:40 +0100

    Updated the nebula experiment, can now apply a seed, width, height and pixleScale to make looking for nice images quick. once you find a nice seed, set teh pixleScale to 1 and the widthxheight of your required image.

--- source/experimental/x.nebula.js
+++ source/experimental/x.nebula.js
@@ -13,6 +13,7 @@ requirejs(['Noise', 'Random',
 	function (Noise, Random) {
 
 	seedRandom = new Random.SeedRandom();
+
 	var prng = function () {
 		return seedRandom.random();
 	};
@@ -37,7 +38,7 @@ requirejs(['Noise', 'Random',
 		var r,
 		g,
 		b;
-		a=a==undefined?1:a;
+		a = a == undefined ? 1 : a;
 
 		if (s == 0) {
 			r = g = b = l; // achromatic
@@ -90,7 +91,7 @@ requirejs(['Noise', 'Random',
 		}
 	}
 
-	var floatArrayToImageDataUint8 = function (floatArray, imageDataUint8) {
+	function floatArrayToImageDataUint8(floatArray, imageDataUint8) {
 		var i,
 		j,
 		l,
@@ -105,14 +106,14 @@ requirejs(['Noise', 'Random',
 		}
 	}
 
-	var floatArrayToCanvas = function (floatArray, canvas) {
+	function floatArrayToCanvas(floatArray, canvas) {
 		imageDataUint8 = new Uint8ClampedArray(canvas.width * canvas.height * 4);
 		floatArrayToImageDataUint8(floatArray, imageDataUint8);
 		var context = canvas.getContext("2d");
 		context.putImageData(new ImageData(imageDataUint8, canvas.width, canvas.height), 0, 0);
 	}
 
-	var colourArrayToImageDataUint8 = function (colourArray, imageDataUint8) {
+	function colourArrayToImageDataUint8(colourArray, imageDataUint8) {
 		var i,
 		j,
 		l,
@@ -126,14 +127,14 @@ requirejs(['Noise', 'Random',
 		}
 	}
 
-	var colourArrayToCanvas = function (colourArray, canvas) {
+	function colourArrayToCanvas(colourArray, canvas) {
 		imageDataUint8 = new Uint8ClampedArray(canvas.width * canvas.height * 4);
 		colourArrayToImageDataUint8(colourArray, imageDataUint8);
 		var context = canvas.getContext("2d");
 		context.putImageData(new ImageData(imageDataUint8, canvas.width, canvas.height), 0, 0);
 	}
 
-	var mixToCanvasToCanvas = function (sourceCanvas, destinationCanvas) {
+	function mixToCanvasToCanvas(sourceCanvas, destinationCanvas) {
 		var context = destinationCanvas.getContext("2d");
 		context.drawImage(sourceCanvas, 0, 0);
 	}
@@ -162,12 +163,8 @@ requirejs(['Noise', 'Random',
 		var pwr = pwHL; /* starts with i=1 instead of 0 */
 
 		var d = getDistortion(x, y, z, distortion, distortionScale, distortfunk);
-		//x = d.x;
-		//y = d.y;
-		//z = d.z;
 		/* first unscaled octave of function; later octaves are scaled */
 		value = offset + noisefunc(d.x, d.y, d.z);
-		//value = offset + noisefunc(x, y, z);
 		x *= lacunarity;
 		y *= lacunarity;
 		z *= lacunarity;
@@ -200,70 +197,7 @@ requirejs(['Noise', 'Random',
 		return min + prng() * range;
 	}
 
-	function generateNebulaDensity(canvas, s) {
-		var m = new Float32Array(canvas.width * canvas.height);
-		for (var x = 0, j = 0; x < canvas.height; x++) {
-			for (var y = 0; y < canvas.width; y++, j++) {
-				m[j] = nebulaDensityNoise(s.dx + x / s.scale, s.dy + y / s.scale, 0, s.h, s.lacunarity, s.octaves, s.offset, Noise.Blender.voronoi_F1, s.distortion, s.distortionScale, Noise.perlin2);
-				m[j] = Math.pow(m[j],s.exponent);
-			}
-		}
-		return m;
-	}
-
-	function generateNebula(canvas, nebulaDensity, s) {
-		var m = new Array(canvas.width * canvas.height); //new Float32Array(s.width * s.height);
-
-		for (var x = 0, j = 0; x < canvas.height; x++) {
-			for (var y = 0; y < canvas.width; y++, j++) {
-				m[j] = hslToColour(s.h,s.s,(nebulaDensity[j]*0.5+0.5)*s.l,nebulaDensity[j]*s.a);//new Colour(nebulaDensity[j]*s.r, nebulaDensity[j]*s.g, nebulaDensity[j]*s.b, nebulaDensity[j]*s.a);
-				//m[j] = new Colour(s.r, s.g, s.b, nebulaDensity[j]*s.a);
-			}
-		}
-		return m;
-	};
-
-	var settings = {
-		star: {
-			density: randomBetween(0.01, 0.05),
-			brightness: randomBetween(0.1, 0.2)
-		},
-	};
-
-	var nebulaCount = 3;
-	for (var i=1; i<=nebulaCount;i++) {
-		settings['nebulaDensity'+i] = {
-			scale: randomBetween(100,2000), //1 / randomBetween(200, 5000),
-			h: randomBetween(0.1, 0.5),
-			lacunarity: 1.5,
-			octaves: 8,
-			dx: randomBetween(0, 500),
-			dy: randomBetween(0, 500),
-			exponent: 1+prng()*2,
-			offset: randomBetween(-0.2, 0.2),
-			distortion: randomBetween(1, 2),
-			distortionScale: randomBetween(1, 2),
-		};
-		settings['nebula'+i] = {
-			h: randomBetween(0, 1),
-			s: randomBetween(0.5, 1),
-			l: randomBetween(0, 1),
-			a: randomBetween(0, 1),
-		};
-	}
-	
-	var canvasNames = ['stars', 'nebulaDensity1', 'nebulaDensity2', 'nebulaDensity3', 'nebula1', 'nebula2', 'nebula3', 'main'];
-	var c = {};
-	var l = {};
-	for (var i = 0; i < canvasNames.length; i++) {
-		var name = canvasNames[i];
-		var canvas = document.getElementById(name);
-		c[name] = canvas;
-	}
-
-	// Random stars
-	l.stars = (
-		function (canvas, s) {
+	function generateStars(canvas, s) {
 		var wxh = canvas.width * canvas.height;
 		var count = Math.round(wxh * s.density);
 		var m = new Array(wxh);
@@ -276,37 +210,202 @@ requirejs(['Noise', 'Random',
 		}
 
 		return m;
-	})(c.stars, settings.star);
-	colourArrayToCanvas(l.stars, c.stars);
+	}
 
-	// --------------
-	l.nebulaDensity1 = generateNebulaDensity(c.nebulaDensity1, settings.nebulaDensity1);
-	normalizeFloatArray(l.nebulaDensity1, 0, 1);
-	floatArrayToCanvas(l.nebulaDensity1, c.nebulaDensity1);
+	function generateNebulaDensity(canvas, s) {
+		var m = new Float32Array(canvas.width * canvas.height);
+		for (var x = 0, j = 0; x < canvas.height; x++) {
+			for (var y = 0; y < canvas.width; y++, j++) {
+				m[j] = nebulaDensityNoise(s.dx + x / s.scale, s.dy + y / s.scale, 0, s.h, s.lacunarity, s.octaves, s.offset, Noise.Blender.voronoi_F1, s.distortion, s.distortionScale, Noise.perlin2);
+				m[j] = Math.pow(m[j], s.exponent);
+			}
+		}
+		return m;
+	}
 
-	l.nebulaDensity2 = generateNebulaDensity(c.nebulaDensity2, settings.nebulaDensity2);
-	normalizeFloatArray(l.nebulaDensity2, 0, 1);
-	floatArrayToCanvas(l.nebulaDensity2, c.nebulaDensity2);
+	function generateNebula(canvas, nebulaDensity, s) {
+		var m = new Array(canvas.width * canvas.height); //new Float32Array(s.width * s.height);
 
-	l.nebulaDensity3 = generateNebulaDensity(c.nebulaDensity3, settings.nebulaDensity3);
-	normalizeFloatArray(l.nebulaDensity3, 0, 1);
-	floatArrayToCanvas(l.nebulaDensity3, c.nebulaDensity3);
-	// --------------
+		for (var x = 0, j = 0; x < canvas.height; x++) {
+			for (var y = 0; y < canvas.width; y++, j++) {
+				m[j] = hslToColour(s.h, s.s, (nebulaDensity[j] * 0.5 + 0.5) * s.l, nebulaDensity[j] * s.a); //new Colour(nebulaDensity[j]*s.r, nebulaDensity[j]*s.g, nebulaDensity[j]*s.b, nebulaDensity[j]*s.a);
+				//m[j] = new Colour(s.r, s.g, s.b, nebulaDensity[j]*s.a);
+			}
+		}
+		return m;
+	};
 
-	// --------------
-	l.nebula1 = generateNebula(c.nebula1, l.nebulaDensity1, settings.nebula1);
-	colourArrayToCanvas(l.nebula1, c.nebula1);
+	function getQueryVars() {
+		var r = {};
+		var search = window.location.search.substring(1);
+		var keyVars = search.split("&");
+		for (var i = 0; i < keyVars.length; i++) {
+			var pair = keyVars[i].split("=");
+			r[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1]);
+		}
+		return r;
+	}
 
-	l.nebula2 = generateNebula(c.nebula2, l.nebulaDensity2, settings.nebula2);
-	colourArrayToCanvas(l.nebula2, c.nebula2);
+	function getRandomString(length, base) {
+		length = length ? length : 8;
+		base = base ? base : 16;
+		var pow = Math.pow(base, length) - 1;
+		var i = pow + Math.round(Math.random() * pow);
+		var r = i.toString(base);
+		r = r.substring(r.length - length);
+		return r;
+	}
 
-	l.nebula3 = generateNebula(c.nebula3, l.nebulaDensity3, settings.nebula3);
-	colourArrayToCanvas(l.nebula3, c.nebula3);
-	// --------------
+	function addLittleCanvas(name, container, settings) {
+		var c = document.createElement('canvas');
+		c.width = settings.realWidth;
+		c.height = settings.realHeight;
+		//c.style.width = settings.lWidth;
+		//c.style.height = settings.lHeight;
+		c.id = name;
+		container.appendChild(c);
+		return c;
+	}
 
+	function generateConfiguration(settings) {
+		settings.seed = undefined;
 
-	mixToCanvasToCanvas(c.stars, c.main);
-	mixToCanvasToCanvas(c.nebula1, c.main);
-	mixToCanvasToCanvas(c.nebula2, c.main);
-	mixToCanvasToCanvas(c.nebula3, c.main);
+		if (typeof(queryVars['seed']) !== 'undefined') {
+			settings.seed = queryVars['seed'];
+		}
+		if (settings.seed == undefined || settings.seed == '') {
+			settings.seed = getRandomString();
+		}
+
+		settings.pixleScale = 1;
+
+		if (typeof(queryVars['pixleScale']) !== 'undefined') {
+			settings.pixleScale = queryVars['pixleScale'];
+		}
+		if (settings.pixleScale == undefined || settings.pixleScale == '') {
+			settings.pixleScale = 1;
+		}
+
+		if (typeof(queryVars['width']) !== 'undefined') {
+			settings.width = queryVars['width'];
+		}
+		if (settings.width == undefined || settings.width == '') {
+			settings.width = window.innerWidth;
+		}
+		
+		if (typeof(queryVars['height']) !== 'undefined') {
+			settings.height = queryVars['height'];
+		}
+		if (settings.height == undefined || settings.height == '') {
+			settings.height = Math.floor(window.innerHeight * 0.9);
+		}
+		
+		settings.realWidth = Math.floor(settings.width / settings.pixleScale);
+		settings.realHeight = Math.floor(settings.height / settings.pixleScale);
+
+		seedRandom.setSeed(settings.seed);
+		seedOutput = document.getElementById("seed");
+		seedOutput.innerText = '?width=' + settings.width + '&height=' + settings.height + '&pixleScale=' + settings.pixleScale + '&seed=' + settings.seed;
+
+		settings.nebulaCount = randomBetween(1, 5);
+		settings.stars = {
+			density: randomBetween(0.01, 0.05),
+			brightness: randomBetween(0.1, 0.2)
+		};
+
+		for (var i = 1; i <= settings.nebulaCount; i++) {
+			settings['nebulaDensity' + i] = {
+				scale: randomBetween(100, 2000) / settings.pixleScale,
+				h: randomBetween(0.1, 0.5),
+				lacunarity: randomBetween(1.2, 2),
+				octaves: Math.floor(randomBetween(3, 8)),
+				dx: randomBetween(0, 500),
+				dy: randomBetween(0, 500),
+				exponent: 1 + prng() * 2,
+				offset: randomBetween(-0.2, 0.2),
+				distortion: randomBetween(1, 2),
+				distortionScale: randomBetween(1, 2),
+			};
+			settings['nebula' + i] = {
+				h: randomBetween(0, 1),
+				s: randomBetween(0.5, 1),
+				l: randomBetween(0, 1),
+				a: randomBetween(0, 1),
+			};
+		}
+	}
+
+	// --------------------------------------------
+
+	var settings = {};
+	var c = {};
+	var l = {};
+
+	// --------------------------------------------
+
+	var queryVars = getQueryVars();
+
+	// --------------------------------------------
+
+	generateConfiguration(settings);
+
+	// --------------------------------------------
+
+	var output = document.getElementById("output");
+	c.output = output;
+
+	output.width = settings.realWidth;
+	output.height = settings.realHeight;
+
+	output.style.width = settings.width + 'px';
+	output.style.height = settings.height + 'px';
+
+	settings.lWidth = Math.floor(window.innerWidth / 10) + 'px'; //(settings.width / (10 / settings.pixleScale)) + 'px';
+	settings.lHeight = Math.floor(window.innerHeight / 10) + 'px'; // (settings.height / (10 / settings.pixleScale)) + 'px';
+
+	var container = document.getElementById('list');
+
+	// --------------------------------------------
+
+	c.stars = addLittleCanvas('stars', container, settings);
+
+	for (var i = 1; i <= settings.nebulaCount; i++) {
+		c['nebulaDensity' + i] = addLittleCanvas('nebulaDensity' + i, container, settings);
+		c['nebula' + i] = addLittleCanvas('nebula' + i, container, settings);
+	}
+
+	// --------------------------------------------
+
+	var callbacks = [];
+
+	callbacks.push(function () {
+		l.stars = generateStars(c.stars, settings.stars);
+		colourArrayToCanvas(l.stars, c.stars);
+		mixToCanvasToCanvas(c.stars, c.output);
+	});
+
+	for (var i = 1; i <= settings.nebulaCount; i++) {
+		(function (x) {
+			callbacks.push(function () {
+				l['nebulaDensity' + x] = generateNebulaDensity(c['nebulaDensity' + x], settings['nebulaDensity' + x]);
+				normalizeFloatArray(l['nebulaDensity' + x], 0, 1);
+				floatArrayToCanvas(l['nebulaDensity' + x], c['nebulaDensity' + x]);
+			});
+			callbacks.push(function () {
+				l['nebula' + x] = generateNebula(c['nebula' + x], l['nebulaDensity' + x], settings['nebula' + x]);
+				colourArrayToCanvas(l['nebula' + x], c['nebula' + x]);
+				mixToCanvasToCanvas(c['nebula' + x], c.output);
+			});
+		})(i);
+	}
+
+	function execCallbacks() {
+		var funk = callbacks.shift();
+		if (funk) {
+			funk();
+			setTimeout(execCallbacks, 100);
+		}
+	}
+	setTimeout(execCallbacks, 100);
+
 });
